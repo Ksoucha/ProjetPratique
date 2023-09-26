4177b1a8fbfd97dd956613889974f6bb7a66e8c7
Isaie Bourget <isaie.bourget@lcieducation.net>
2023-09-26 10:50:09 -0400

    Ajout de metre yards ounces grams

--- ProjetOutils/Metric.cs
+++ ProjetOutils/Metric.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,54 @@ namespace ProjetOutils
 {
     internal class Metric
     {
+        public Metric()
+        {
+            double grams;
+            double mètre;
+            double litres;
+            double yards;
+            double ounces;
+            double gallons;
+
+            Console.WriteLine("\n\t\t\t\tBienvenue au programme de " +
+                "conertisseur de Metric et de Imperial");
+            Console.WriteLine("\t\t\t\t Entre mètre pour la convertire en yards ");
+            Console.WriteLine("\t\t\t\t Entre yards pour la convertire en mètre ");
+            Console.WriteLine("\t\t\t\t Entre grams pour la convertire en ounces ");
+            Console.WriteLine("\t\t\t\t Entre ounces pour la convertire en grams ");
+            Console.WriteLine("\t\t\t\t Entre litre pour la convertire en gallons ");
+            Console.WriteLine("\t\t\t\t Entre gallons pour la convertire en litre ");
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line.ToLower() == "metre" || line.ToLower() == "Metre" ||
+                    line.ToLower() == "mètre" || line.ToLower() == "Métre")
+                {
+                    Console.WriteLine("Bienvenuau au convertisseur de mètre en yards.");
+                    Console.WriteLine("Qu'elle est la convertission que vous voulez faire ?");
+
+                    mètre = Convert.ToDouble(Console.ReadLine());
+                    yards = mètre * 1.0936;
+                    Console.WriteLine($"La conversion en years est: {yards} yd");
+                    Console.ReadKey();
+                }
+                else if (line.ToLower() == "yards" || line.ToLower() == "Yards")
+                {
+                    Console.WriteLine("Bienvenuau au convertisseur de " +
+                        "Yards en mètre");
+                    Console.WriteLine("Qu'elle est la convertission que vous voulez faire ?");
+
+
+                    yards = Convert.ToDouble(Console.ReadLine());
+                    mètre = yards * 1.09144; 
+                    Console.WriteLine($"La conversion en mètre est: {yards} CAN");
+                    Console.ReadKey();
+
+                }
+            }
+        }
+
+
     }
 }
--- ProjetOutils/Program.cs
+++ ProjetOutils/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ProjetOutils;
+using System.ComponentModel.Design;
+
+Console.WriteLine("\t\t\t\t\t\tVous aimeriez accéder à l'option " +
+    "\n\t\t\t\t\t\t\t Monnaie ou Metric ou bien, Quiz");
+
+while (true)
+{
+    string line = Console.ReadLine();
+    if (line.ToLower() == "monnaie" || line.ToLower() == "monnais")
+    {
+        new Monnaie();
+    }
+    else if (line.ToLower() == "Quiz" || line.ToLower() == "quiz")
+    {
+        new Quiz();
+    }
+    else if (line.ToLower() == "metric" || line.ToLower() == "Metric")
+    {
+        new Metric();
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("S'il vous plait écriver la bonne chose");
+        Console.ResetColor();
+    }
+}
